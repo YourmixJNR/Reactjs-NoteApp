558c9e24293ef9aa9a99d987e55db3d564aba1b9
Michael Victor <radmakson@gmail.com>
2023-10-18 16:26:25 +0100

    fix: bugs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './components/Header';
-import Notes from './pages/Notes';
+import Notes from './pages/NotesPage';
+import Note from "./pages/NotePage";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Notes />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Notes />} />
+          <Route path="/note" element={<Note />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 

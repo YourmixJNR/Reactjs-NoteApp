1f48cc08380033dc4c6855580b8732d9cdd60832
Michael Victor <radmakson@gmail.com>
2023-10-18 14:30:23 +0100

    Body - Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Header from './components/Header';
+import Body from './components/Body';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Body />
+      <Footer />
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
+import React from "react";
 
 const Header = () => {
     return (
         <div>
-            Hello World
+            <h1>Hello World</h1>
         </div>
     )
 };
